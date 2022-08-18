0db204685e9aa3299838781bae70e43c0d5696cb
yeungadrian <yeungadrianjy@gmail.com>
2022-08-18 22:33:48 +0100

    Creating config file, tidying up naming, colors

--- aurora/frontend/app/modules/backtest.py
+++ aurora/frontend/app/modules/backtest.py
@@ -1,69 +1,36 @@
 from datetime import datetime
+from typing import List
 
 import altair as alt
 import pandas as pd
 import requests
 import streamlit as st
+from modules.config import Config
 from modules.funds import get_funds
+from pydantic import BaseModel
 
-summary_map = {
-    "cagr": "CAGR",
-    "std_m": "Monthly standard deviation",
-    "max_drawdown": "Maximum drawdown",
-    "sharpe_ratio": "Sharpe ratio",
-    "sortino_ratio": "Sortino ratio",
-    "market_correlation": "Market correlation",
-}
-
-metric_map = {
-    "cagr": "CAGR",
-    "std_m": "Monthly standard deviation",
-    "max_drawdown": "Maximum drawdown",
-    "sharpe_ratio": "Sharpe ratio",
-    "sortino_ratio": "Sortino ratio",
-    "market_correlation": "Market correlation",
-    "arithmetic_mean_m": "Monthly Arithmetic mean",
-    "arithmetic_mean_y": "Monthly Arithmetic mean",
-    "geometric_mean_m": "Monthly Arithmetic mean",
-    "geometric_mean_y": "Monthly Arithmetic mean",
-    "std_m": "Monthly standard deviation",
-    "std_downside_m": "Monthly downside standard deviation",
-    "alpha": "Alpha",
-    "beta": "Beta",
-    "r_squared": "R squared",
-    "cagr": "CAGR",
-    "treynor_ratio": "Treynor ratio",
-    "calmar_ratio": "Calmar ratio",
-    "active_return": "Active return",
-    "tracking_error": "Tracking error",
-    "information_ratio": "Information ratio",
-    "upside_capture_ratio": "Upside capture ratio",
-    "downside_capture_ratio": "Downside capture ratio",
-    "capture_ratio": "Capture ratio",
-}
-
-format_dict = {
-    "Maximum drawdown": "{:.2%}",
-    "Monthly standard deviation": "{:.2%}",
-    "Annual arithmetic mean": "{:.2%}",
-    "Monthly arithmetic mean": "{:.2%}",
-    "CAGR": "{:.2%}",
-    "Sharpe ratio": "{:.2f}",
-    "Sortino ratio": "{:.2f}",
-    "Market correlation": "{:.2f}",
-}
-
-colors = {
-    "first": "#ED254E",
-    "second": "#1E2019",
-    "third": "#361D2E",
-    "fourth": "#3C6997",
-    "fifth": "#EEE2DF",
-}
+column_map = Config().column_map()
+format_pct = Config().format_pct()
+format_dec = Config().format_dec()
+colors = Config().colors()
 
 
-class Backtest:
+class Backtest(BaseModel):
+    short_metrics: List = [
+        "cagr",
+        "std_m",
+        "max_drawdown",
+        "sharpe_ratio",
+        "sortino_ratio",
+        "market_correlation",
+    ]
+
     def sidebar(self):
+        """Sidebar for streamlit page
+
+        Returns:
+            dict: Portfolio strategy inputs
+        """
 
         available_funds = pd.DataFrame(get_funds())
 
@@ -108,17 +75,38 @@ class Backtest:
                 }
             )
 
-        return start_date, end_date, frequency, portfolio, rebalance
+            backtest_input = {
+                "start_date": start_date,
+                "end_date": end_date,
+                "portfolio": portfolio,
+                "strategy": {"rebalance": rebalance, "rebalance_frequency": frequency},
+            }
+
+        return backtest_input
 
     @st.cache()
-    def get_backtest(self, backtest_input):
+    def backtest_portfolio(self, input):
+        """Backtest portfolio strategy
+
+        Args:
+            input (dict): portfolio strategy
+
+        Returns:
+            dict: portfolio backtest
+        """
+
         url_backtest = "http://localhost:8000/backtest/"
-        backtest_response = requests.post(url=url_backtest, json=backtest_input).json()
+        response = requests.post(url=url_backtest, json=input).json()
 
-        return backtest_response
+        return response
 
-    def portfolio_value_page(self, backtest_response):
-        backtest_portfolio = pd.DataFrame(backtest_response["projection"])
+    def portfolio_value(self, portfolio_projection):
+        """Display portfolio value section
+
+        Args:
+            portfolio_projection (dict): portfolio projection, value & metrics
+        """
+        backtest_portfolio = pd.DataFrame(portfolio_projection["projection"])
         backtest_portfolio["date"] = pd.to_datetime(backtest_portfolio["date"])
 
         backtest_portfolio = backtest_portfolio.rename(
@@ -134,109 +122,150 @@ class Backtest:
                 x="Date",
                 y="Portfolio Value ($)",
                 tooltip=["Date", "Portfolio Value ($)"],
-                color=alt.value(colors["first"]),
+                color=alt.value(colors["fourth"]),
             )
         )
 
         st.altair_chart(value_chart, use_container_width=True)
 
-    def summary_metrics_page(self, backtest_response):
-        metrics = backtest_response["metrics"]["metrics"]
+    def summary_metrics(self, portfolio_projection):
+        """Display summary metrics section. Title and table
+
+        Args:
+            portfolio_projection (_type_): _description_
+        """
+        metrics = portfolio_projection["metrics"]["metrics"]
 
         summary_metrics = {}
 
-        for i in summary_map.keys():
+        for i in self.short_metrics:
             summary_metrics[i] = metrics[i]
 
         summary_metrics = pd.DataFrame(summary_metrics, index=[""])
 
-        summary_metrics = summary_metrics.rename(columns=summary_map)
+        summary_metrics = summary_metrics.rename(columns=column_map)
 
         st.subheader("Summary metrics")
 
         st.markdown("")
         st.write(
-            summary_metrics.style.format(format_dict).hide_index().to_html(),
+            summary_metrics.style.format({**format_pct, **format_dec})
+            .hide_index()
+            .to_html(),
             unsafe_allow_html=True,
         )
         st.markdown("")
 
-    def annual_return(self, backtest_response):
+    def annual_return(self, portfolio_projection):
+        """Annual return section, title, bar chart
+
+        Args:
+            portfolio_projection (dict): backtesting portfolio results
+        """
         st.subheader("Annual returns")
 
-        annual_returns = pd.DataFrame(backtest_response["metrics"]["annual"]["return"])
+        annual_returns = pd.DataFrame(
+            portfolio_projection["metrics"]["annual"]["return"]
+        )
+
+        annual_returns = annual_returns.rename(
+            columns={"portfolio_returns": "Portfolio returns", "date": "Date"}
+        )
 
         return_chart = (
             alt.Chart(annual_returns)
             .mark_bar()
             .encode(
-                x="date:T",
-                y=alt.Y("portfolio_returns", axis=alt.Axis(format="%")),
-                tooltip=["date:T", alt.Tooltip("portfolio_returns", format=".2%")],
-                color=alt.value(colors["second"]),
+                x="Date:N",
+                y=alt.Y("Portfolio returns", axis=alt.Axis(format="%")),
+                tooltip=["Date:N", alt.Tooltip("Portfolio returns", format=".2%")],
+                color=alt.value(colors["fourth"]),
             )
         )
 
         st.altair_chart(return_chart, use_container_width=True)
 
-    def drawdowns(self, backtest_response):
+    def drawdowns(self, portfolio_projection):
+        """Drawdown page, expander, title and line chart
+
+        Args:
+            portfolio_projection (dict): _description_
+        """
         with st.expander("Drawdowns"):
 
             daily_drawdown = pd.DataFrame(
-                backtest_response["metrics"]["daily"]["drawdown"]
+                portfolio_projection["metrics"]["daily"]["drawdown"]
+            )
+
+            daily_drawdown = daily_drawdown.rename(
+                columns={"drawdown": "Daily drawdown", "date": "Date"}
             )
 
             drawdown_chart = (
                 alt.Chart(daily_drawdown)
                 .mark_line()
                 .encode(
-                    x="date:T",
-                    y=alt.Y("drawdown", axis=alt.Axis(format="%")),
-                    tooltip=["date:T", alt.Tooltip("drawdown", format=".2%")],
+                    x="Date:T",
+                    y=alt.Y("Daily drawdown", axis=alt.Axis(format="%")),
+                    tooltip=["Date:T", alt.Tooltip("Daily drawdown", format=".2%")],
                     color=alt.value(colors["fourth"]),
                 )
             )
 
             st.altair_chart(drawdown_chart, use_container_width=True)
 
-    def metrics(self, backtest_response):
+    def metrics(self, portfolio_projection):
+        """Full metrics, expander and table
+
+        Args:
+            portfolio_projection (dict): _description_
+        """
+
         with st.expander("Metrics"):
             st.markdown("")
-            st.write(
-                pd.DataFrame(backtest_response["metrics"]["metrics"], index=[0])
-                .rename(columns=metric_map)
-                .transpose()
-                .reset_index()
-                .style.format(format_dict)
-                .hide_index()
-                .hide_columns()
-                .to_html(),
-                unsafe_allow_html=True,
-            )
-            st.markdown("")
+            pct_metrics = pd.DataFrame(
+                portfolio_projection["metrics"]["metrics"], index=[0]
+            ).rename(columns=column_map)
+            pct_metrics = pct_metrics[format_pct.keys()]
+            dec_metrics = pd.DataFrame(
+                portfolio_projection["metrics"]["metrics"], index=[0]
+            ).rename(columns=column_map)
+            dec_metrics = dec_metrics[format_dec.keys()]
+
+            col1, col2 = st.columns(2)
+            with col1:
+                st.write(
+                    pct_metrics.transpose()
+                    .style.format("{:.2%}")
+                    .hide(axis="columns")
+                    .to_html(),
+                    unsafe_allow_html=True,
+                )
+
+            with col2:
+
+                st.write(
+                    dec_metrics.transpose()
+                    .style.format("{:.2f}")
+                    .hide(axis="columns")
+                    .to_html(),
+                    unsafe_allow_html=True,
+                )
+                st.markdown("")
 
     def display(self):
+        """Display backtest page"""
 
-        start_date, end_date, frequency, portfolio, rebalance = self.sidebar()
+        backtest_input = self.sidebar()
 
         st.title("Portfolio Backtesting")
 
-        if len(portfolio):
-            backtest_input = {
-                "start_date": start_date,
-                "end_date": end_date,
-                "portfolio": portfolio,
-                "strategy": {"rebalance": rebalance, "rebalance_frequency": frequency},
-            }
+        if len(backtest_input["portfolio"]):
 
-            backtest_response = self.get_backtest(backtest_input)
+            portfolio_projection = self.backtest_portfolio(backtest_input)
 
-            self.portfolio_value_page(backtest_response)
-
-            self.summary_metrics_page(backtest_response)
-
-            self.annual_return(backtest_response)
-
-            self.metrics(backtest_response)
-
-            self.drawdowns(backtest_response)
+            self.portfolio_value(portfolio_projection)
+            self.summary_metrics(portfolio_projection)
+            self.annual_return(portfolio_projection)
+            self.metrics(portfolio_projection)
+            self.drawdowns(portfolio_projection)
---
+++ aurora/frontend/app/modules/config.py
@@ -0,0 +1,67 @@
+class Config:
+    def colors(self):
+        return {
+            "first": "#ED254E",
+            "second": "#1E2019",
+            "third": "#361D2E",
+            "fourth": "#3C6997",
+            "fifth": "#EEE2DF",
+        }
+
+    def format_pct(self):
+        return {
+            "CAGR": "{:.2%}",
+            "Monthly standard deviation": "{:.2%}",
+            "Maximum drawdown": "{:.2%}",
+            "Monthly arithmetic mean": "{:.2%}",
+            "Annual arithmetic mean": "{:.2%}",
+            "Monthly geomtric mean": "{:.2%}",
+            "Annual geomtric mean": "{:.2%}",
+            "Monthly downside standard deviation": "{:.2%}",
+            "R squared": "{:.2%}",
+        }
+
+    def format_dec(self):
+        return {
+            "Alpha": "{:.2f}",
+            "Beta": "{:.2f}",
+            "Sharpe ratio": "{:.2f}",
+            "Sortino ratio": "{:.2f}",
+            "Market correlation": "{:.2f}",
+            "Treynor ratio": "{:.2f}",
+            "Calmar ratio": "{:.2f}",
+            "Active return": "{:.2f}",
+            "Tracking error": "{:.2f}",
+            "Information ratio": "{:.2f}",
+            "Upside capture ratio": "{:.2f}",
+            "Downside capture ratio": "{:.2f}",
+            "Capture ratio": "{:.2f}",
+        }
+
+    def column_map(self):
+        return {
+            "cagr": "CAGR",
+            "std_m": "Monthly standard deviation",
+            "max_drawdown": "Maximum drawdown",
+            "sharpe_ratio": "Sharpe ratio",
+            "sortino_ratio": "Sortino ratio",
+            "market_correlation": "Market correlation",
+            "arithmetic_mean_m": "Monthly arithmetic mean",
+            "arithmetic_mean_y": "Annual arithmetic mean",
+            "geometric_mean_m": "Monthly geomtric mean",
+            "geometric_mean_y": "Annual geomtric mean",
+            "std_m": "Monthly standard deviation",
+            "std_downside_m": "Monthly downside standard deviation",
+            "alpha": "Alpha",
+            "beta": "Beta",
+            "r_squared": "R squared",
+            "cagr": "CAGR",
+            "treynor_ratio": "Treynor ratio",
+            "calmar_ratio": "Calmar ratio",
+            "active_return": "Active return",
+            "tracking_error": "Tracking error",
+            "information_ratio": "Information ratio",
+            "upside_capture_ratio": "Upside capture ratio",
+            "downside_capture_ratio": "Downside capture ratio",
+            "capture_ratio": "Capture ratio",
+        }
