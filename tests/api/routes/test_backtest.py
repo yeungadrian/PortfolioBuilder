d7ec6be2150b1c83fd257aacba9abbc27bb6998b
yeungadrian <yeungadrianjy@gmail.com>
2024-07-01 21:18:05 +0100

    Adding tests for validation

--- tests/api/routes/test_backtest.py
+++ tests/api/routes/test_backtest.py
@@ -23,3 +23,20 @@ def test_backtest(*, client: TestClient) -> None:
     assert response.json()[1]["portfolio_value"] == pytest.approx(211.5677910878)
 
     assert response.json()[2]["portfolio_value"] == pytest.approx(207.67257285005837)
+
+
+def test_backtest_validation_error(*, client: TestClient) -> None:
+    body = {
+        "start_date": "2023-11-30",
+        "end_date": "2024-01-31",
+        "portfolio": [
+            {"amount": 100, "id": "fake_fund"},
+            {
+                "amount": 100,
+                "id": "vanguard-uk-inflation-linked-gilt-index-fund-gbp-acc",
+            },
+        ],
+    }
+
+    response = client.post("/backtest/", json=body)
+    assert response.status_code == 404
--- tests/api/routes/test_funds.py
+++ tests/api/routes/test_funds.py
@@ -13,3 +13,8 @@ def test_funds_sedol_ok(*, client: TestClient) -> None:
     assert response.status_code == 200
     assert response.json()["id"] == "vanguard-us-equity-index-fund-gbp-acc"
     assert response.json()["currency_code"] == "GBP"
+
+
+def test_unds_sedol_validation_error(*, client: TestClient) -> None:
+    response = client.get("/funds/FAKESEDOL/")
+    assert response.status_code == 404
