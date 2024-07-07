f31da4621e1ae41154d97e9d96044aff12acac92
yeungadrian <yeungadrianjy@gmail.com>
2024-07-07 14:55:09 +0100

    increase max line char

--- app/api/routes/backtest.py
+++ app/api/routes/backtest.py
@@ -10,10 +10,7 @@ router = APIRouter()
 def validate_ids(ids: list[str]) -> None:
     """Validate ids provided."""
     avaliable_ids = (
-        pl.scan_parquet(data_settings.fund_details)
-        .filter(pl.col("id").is_in(ids))
-        .collect()["id"]
-        .to_list()
+        pl.scan_parquet(data_settings.fund_details).filter(pl.col("id").is_in(ids)).collect()["id"].to_list()
     )
 
     not_avaliable = [_id for _id in ids if _id not in avaliable_ids]
@@ -29,22 +26,15 @@ def validate_ids(ids: list[str]) -> None:
 def backtest_portfolio(backtest_scenario: BacktestScenario) -> list[BacktestDetail]:
     """Backtest portfolio."""
     # TODO: start_date / end_date is assumed to be month_ends
-    holdings = {
-        holding["id"]: holding["amount"]
-        for holding in backtest_scenario.model_dump()["portfolio"]
-    }
+    holdings = {holding["id"]: holding["amount"] for holding in backtest_scenario.model_dump()["portfolio"]}
     ids = list(holdings.keys())
 
     validate_ids(ids)
 
     holding_returns = (
-        pl.scan_parquet(data_settings.holding_returns)
+        pl.scan_parquet(data_settings.fund_returns)
         .filter(pl.col("id").is_in(ids))
-        .filter(
-            pl.col("date").is_between(
-                backtest_scenario.start_date, backtest_scenario.end_date
-            )
-        )
+        .filter(pl.col("date").is_between(backtest_scenario.start_date, backtest_scenario.end_date))
         .collect()
     )
 
@@ -55,9 +45,7 @@ def backtest_portfolio(backtest_scenario: BacktestScenario) -> list[BacktestDeta
         .alias("monthly_return")
     )
 
-    holding_returns = holding_returns.with_columns(
-        pl.col("monthly_return") + 1
-    ).with_columns(
+    holding_returns = holding_returns.with_columns(pl.col("monthly_return") + 1).with_columns(
         pl.col("monthly_return").cum_prod().over("id").alias("cum_prod") - 1.0
     )
 
@@ -65,18 +53,14 @@ def backtest_portfolio(backtest_scenario: BacktestScenario) -> list[BacktestDeta
     _backtest_summary = _backtest_summary.with_columns(
         [((pl.col(i) + 1.0) * j).alias(i) for i, j in holdings.items()]
     )
-    _backtest_summary = _backtest_summary.with_columns(
-        pl.sum_horizontal(ids).alias("portfolio_value")
-    )
+    _backtest_summary = _backtest_summary.with_columns(pl.sum_horizontal(ids).alias("portfolio_value"))
 
     backtest_summary = [
         BacktestDetail(
             date=row["date"],
             portfolio_value=row["portfolio_value"],
             holdings=[
-                {"id": _id, "amount": amount}
-                for _id, amount in row.items()
-                if _id not in ["date", "portfolio_value"]
+                {"id": _id, "amount": amount} for _id, amount in row.items() if _id not in ["date", "portfolio_value"]
             ],
         )
         for row in _backtest_summary.to_dicts()
