b2e67db8595e19e8e67e2e0d8310bd772c0e994b
yeungadrian <yeungadrianjy@gmail.com>
2024-07-08 20:59:40 +0100

    var consistency

--- app/api/routes/backtest.py
+++ app/api/routes/backtest.py
@@ -31,25 +31,25 @@ def backtest_portfolio(backtest_scenario: BacktestScenario) -> list[BacktestDeta
 
     validate_ids(ids)
 
-    holding_returns = (
+    security_returns = (
         pl.scan_parquet(data_settings.fund_returns)
         .filter(pl.col("id").is_in(ids))
         .filter(pl.col("date").is_between(backtest_scenario.start_date, backtest_scenario.end_date))
         .collect()
     )
 
-    holding_returns = holding_returns.with_columns(
+    security_returns = security_returns.with_columns(
         pl.when(pl.col("date") == pl.col("date").min())
         .then(0)
         .otherwise(pl.col("monthly_return"))
         .alias("monthly_return")
     )
 
-    holding_returns = holding_returns.with_columns(pl.col("monthly_return") + 1).with_columns(
+    security_returns = security_returns.with_columns(pl.col("monthly_return") + 1).with_columns(
         pl.col("monthly_return").cum_prod().over("id").alias("cum_prod") - 1.0
     )
 
-    _backtest_summary = holding_returns.pivot(on="id", values="cum_prod", index="date")
+    _backtest_summary = security_returns.pivot(on="id", values="cum_prod", index="date")
     _backtest_summary = _backtest_summary.with_columns(
         [((pl.col(i) + 1.0) * j).alias(i) for i, j in holdings.items()]
     )
