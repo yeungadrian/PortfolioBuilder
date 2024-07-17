194e7487224154600d3c45e5f8795f1e77cc5739
yeungadrian <yeungadrianjy@gmail.com>
2024-07-17 22:45:31 +0100

    delete workspaces

--- frontend/src/components/ui/navigation/sidebar.tsx
+++ frontend/src/components/ui/navigation/sidebar.tsx
@@ -10,10 +10,6 @@ import {
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import MobileSidebar from "./MobileSidebar"
-import {
-  WorkspacesDropdownDesktop,
-  WorkspacesDropdownMobile,
-} from "./SidebarWorkspacesDropdown"
 import { UserProfileDesktop, UserProfileMobile } from "./UserProfile"
 
 const navigation = [
@@ -62,7 +58,6 @@ export function Sidebar() {
       {/* sidebar (lg+) */}
       <nav className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
         <aside className="flex grow flex-col gap-y-6 overflow-y-auto border-r border-gray-200 bg-white p-4 dark:border-gray-800 dark:bg-gray-950">
-          <WorkspacesDropdownDesktop />
           <nav
             aria-label="core navigation links"
             className="flex flex-1 flex-col space-y-10"
@@ -121,7 +116,6 @@ export function Sidebar() {
       </nav>
       {/* top navbar (xs-lg) */}
       <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center justify-between border-b border-gray-200 bg-white px-2 shadow-sm sm:gap-x-6 sm:px-4 lg:hidden dark:border-gray-800 dark:bg-gray-950">
-        <WorkspacesDropdownMobile />
         <div className="flex items-center gap-1 sm:gap-2">
           <UserProfileMobile />
           <MobileSidebar />
