91cf6f49b63f047ea197f5883dc9db83c5af0bc6
yeungadrian <yeungadrianjy@gmail.com>
2024-07-23 22:44:10 +0100

    Adding cache to requests

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 import json
 import os
 from datetime import datetime
+from typing import Any
 
 import altair as alt
 import httpx
@@ -21,10 +22,38 @@ OPTIMISATION_IDS = [
 ]
 
 
+@st.cache_data(ttl="7d")
+def get_funds() -> Any:
+    """Get funds."""
+    r = httpx.get(f"{BASE_URL}funds/all/")
+    return r.json()
+
+
+@st.cache_data(ttl="7d")
+def backtest(start_date: str, end_date: str, portfolio: str) -> Any:
+    """Get funds."""
+    r = httpx.post(
+        f"{BASE_URL}backtest/",
+        headers={"Content-Type": "application/json"},
+        json={"start_date": start_date, "end_date": end_date, "portfolio": json.loads(portfolio)},
+    )
+    return r.json()
+
+
+@st.cache_data(ttl="7d")
+def get_efficient_fronter(start_date: str, end_date: str, ids: str, n_portfolios: int) -> Any:
+    """Get funds."""
+    r = httpx.post(
+        f"{BASE_URL}optimisation/efficient-frontier?n_portfolios={n_portfolios}",
+        headers={"Content-Type": "application/json"},
+        json={"start_date": start_date, "end_date": end_date, "ids": json.loads(ids)},
+    )
+    return r.json()
+
+
 def screener_page() -> None:
     """Screener page."""
-    r = httpx.get(f"{BASE_URL}funds/all/")
-    df = pd.DataFrame(r.json())
+    df = pd.DataFrame(get_funds())
     return_cols = df.columns[df.columns.str.contains("returns")].tolist()
     df = df.style.format({i: "{:,.2%}".format for i in return_cols})
 
@@ -37,22 +66,21 @@ def backtest_page() -> None:
     start_date = st.sidebar.date_input("Start date", value=datetime(2018, 1, 1)).strftime("%Y-%m-%d")
     end_date = st.sidebar.date_input("End date", value=datetime(2024, 1, 1)).strftime("%Y-%m-%d")
     portfolio = st.sidebar.text_area("Portfolio setup", value=json.dumps(BACKTEST_EXAMPLE), height=400)
-    r = httpx.post(
-        f"{BASE_URL}backtest/",
-        headers={"Content-Type": "application/json"},
-        json={"start_date": start_date, "end_date": end_date, "portfolio": json.loads(portfolio)},
-    )
+
+    backtest_results = backtest(start_date, end_date, portfolio)
 
     st.title("Portfolio Backtesting")
 
     detailed_holdings = []
-    for i in r.json():
+    for i in backtest_results:
         _result = {}
         for holding in i["holdings"]:
             _result[holding["id"]] = holding["amount"]
         detailed_holdings.append(_result)
 
-    df = pd.concat([pd.DataFrame(r.json())[["date", "portfolio_value"]], pd.DataFrame(detailed_holdings)], axis=1)
+    df = pd.concat(
+        [pd.DataFrame(backtest_results)[["date", "portfolio_value"]], pd.DataFrame(detailed_holdings)], axis=1
+    )
     df = df.melt(id_vars=["date", "portfolio_value"], var_name="fund", value_name="amount")
     chart = (
         alt.Chart(df)
@@ -74,13 +102,7 @@ def optimisation_page() -> None:
     end_date = st.sidebar.date_input("End date", value=datetime(2024, 1, 1)).strftime("%Y-%m-%d")
     ids = st.sidebar.text_area("Funds", value=json.dumps(OPTIMISATION_IDS), height=400)
 
-    r = httpx.post(
-        f"{BASE_URL}optimisation/efficient-frontier?n_portfolios={n_portfolios}",
-        headers={"Content-Type": "application/json"},
-        json={"start_date": start_date, "end_date": end_date, "ids": json.loads(ids)},
-    )
-
-    frontier = r.json()
+    frontier = get_efficient_fronter(start_date, end_date, ids, n_portfolios)
     return_variance = pd.DataFrame(
         [
             {"expected_return": portfolio["expected_return"], "variance": portfolio["variance"]}
