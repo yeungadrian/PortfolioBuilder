22206d8f8ce8891f6323687ea2aa97d694b7c4a1
yeungadrian <yeungadrianjy@gmail.com>
2024-07-23 21:34:28 +0100

    Adding efficient frontier endpoint

--- app/api/routes/optimisation.py
+++ app/api/routes/optimisation.py
@@ -24,7 +24,7 @@ def optimise(
     func: Callable[..., float],
     args: tuple[np.ndarray],
     bounds: tuple[tuple[float, float], ...],
-    constraints: tuple[dict[str, Any]],
+    constraints: tuple[Any, ...],
     initial_weights: np.ndarray,
 ) -> list[float]:
     """Scipy minimize."""
@@ -40,15 +40,11 @@ def optimise(
     return _result
 
 
-def min_volatility(
-    expected_returns: np.ndarray,
-    risk_model: np.ndarray,
-) -> list[float]:
+def min_volatility(expected_returns: np.ndarray, risk_model: np.ndarray, constraints: tuple[Any, ...]) -> list[float]:
     """Use optimisation to find portfolio with minimum std for a given return."""
     n_securities = expected_returns.shape[0]
     args = risk_model
     initial_weights = np.repeat(1.0 / n_securities, n_securities)
-    constraints = ({"type": "eq", "fun": lambda x: np.sum(x) - 1},)
     bounds = tuple((0.0, 1.0) for i in np.nditer(expected_returns))
     _result = optimise(calculate_portfolio_std, args, bounds, constraints, initial_weights)
     return _result
@@ -60,5 +56,28 @@ def mean_variance_optimisation(scenario: OptimisationScenario) -> list[Holding]:
     security_returns = load_returns(scenario.ids, scenario.start_date, scenario.end_date)
     expected_returns = calculate_historical_expected_returns(security_returns, scenario.ids).to_numpy().T
     sample_covariance = calculate_sample_covariance(security_returns.select(pl.col(scenario.ids)).to_numpy())
-    min_vol_portfolio = min_volatility(expected_returns, sample_covariance)
+    constraints = ({"type": "eq", "fun": lambda x: np.sum(x) - 1},)
+    min_vol_portfolio = min_volatility(expected_returns, sample_covariance, constraints)
     return [Holding(id=id, amount=ratio) for id, ratio in zip(scenario.ids, min_vol_portfolio)]
+
+
+@router.post("/efficient-frontier")
+def efficient_frontier(scenario: OptimisationScenario, n_portfolios: int = 5) -> Any:
+    """Generate efficient frontier portfolios."""
+    security_returns = load_returns(scenario.ids, scenario.start_date, scenario.end_date)
+    expected_returns = calculate_historical_expected_returns(security_returns, scenario.ids).to_numpy().T
+    sample_covariance = calculate_sample_covariance(security_returns.select(pl.col(scenario.ids)).to_numpy())
+    efficient_portfolios = []
+    for target_return in np.linspace(min(expected_returns), max(expected_returns), n_portfolios):
+        constraints = (
+            {"type": "eq", "fun": lambda x: np.sum(expected_returns.T * x) - target_return},  # noqa: B023
+            {"type": "eq", "fun": lambda x: np.sum(x) - 1},
+        )
+        min_vol_portfolio = min_volatility(expected_returns, sample_covariance, constraints)
+        portfolio_summary = {
+            "portfolio": [Holding(id=id, amount=ratio) for id, ratio in zip(scenario.ids, min_vol_portfolio)],
+            "expected_return": np.sum(expected_returns.T * min_vol_portfolio),
+            "variance": calculate_portfolio_std(min_vol_portfolio, sample_covariance),
+        }
+        efficient_portfolios.append(portfolio_summary)
+    return efficient_portfolios
--- tests/api/routes/test_optimisation.py
+++ tests/api/routes/test_optimisation.py
@@ -71,3 +71,27 @@ def test_min_vol(*, client: TestClient) -> None:
     assert response.status_code == 200
     assert response.json()[0]["id"] == "vanguard-ftse-100-index-unit-trust-gbp-acc"
     assert response.json()[0]["amount"] == pytest.approx(0.548671633696346)
+
+
+def test_efficient_frontier(*, client: TestClient) -> None:
+    body = {
+        "end_date": "2024-01-01",
+        "ids": [
+            "vanguard-ftse-100-index-unit-trust-gbp-acc",
+            "vanguard-us-equity-index-fund-gbp-acc",
+            "vanguard-uk-long-duration-gilt-index-fund-gbp-acc",
+        ],
+        "start_date": "2018-01-01",
+    }
+
+    response = client.post("/optimisation/efficient-frontier?n_portfolios=3", json=body)
+    assert response.status_code == 200
+    assert response.json()[0]["portfolio"][0]["id"] == "vanguard-ftse-100-index-unit-trust-gbp-acc"
+    assert response.json()[0]["portfolio"][0]["amount"] == pytest.approx(4.732899320939246e-13)
+    assert response.json()[0]["expected_return"] == pytest.approx(-0.05346345590292251)
+    assert response.json()[0]["variance"] == pytest.approx(0.15403819424142662)
+
+    assert response.json()[1]["portfolio"][1]["id"] == "vanguard-us-equity-index-fund-gbp-acc"
+    assert response.json()[1]["portfolio"][1]["amount"] == pytest.approx(0.2794289574642864)
+    assert response.json()[1]["expected_return"] == pytest.approx(0.03387391520563446)
+    assert response.json()[1]["variance"] == pytest.approx(0.1066544373178886)
