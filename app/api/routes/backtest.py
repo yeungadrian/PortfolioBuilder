276c9f3d1255e44bdd77923337b9b30316212f1b
yeungadrian <yeungadrianjy@gmail.com>
2024-10-20 22:22:04 +0100

    rm prints

--- app/api/routes/backtest.py
+++ app/api/routes/backtest.py
@@ -24,19 +24,16 @@ def run_backtesting(backtest_scenario: BacktestScenario) -> pl.DataFrame:
     """Run backtesting."""
     ids = [i.id for i in backtest_scenario.portfolio]
     security_returns = load_returns(ids, backtest_scenario.start_date, backtest_scenario.end_date)
-    print(security_returns)
     # Set return for day 0 to 0.
     security_returns = security_returns.with_columns(
         [pl.when(pl.col("date") == pl.col("date").min()).then(0).otherwise(pl.col(_id)).alias(_id) for _id in ids]
     )
     # Calculate monthly returns
     security_returns = security_returns.with_columns([(pl.col(_id) + 1.0).cum_prod().alias(_id) - 1.0 for _id in ids])
-    print(security_returns)
     security_returns = security_returns.with_columns(
         [((pl.col(holding.id) + 1.0) * holding.amount).alias(holding.id) for holding in backtest_scenario.portfolio]
     )
     security_returns = security_returns.with_columns(pl.sum_horizontal(ids).alias("portfolio_value"))
-    print(security_returns)
     return security_returns
 
 
