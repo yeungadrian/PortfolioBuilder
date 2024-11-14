0b8b454e693384fd237791c5027c49a3a941259d
yeungadrian <yeungadrianjy@gmail.com>
2024-11-14 22:03:32 +0000

    minor perf improvement

--- app/expected_returns.py
+++ app/expected_returns.py
@@ -3,11 +3,11 @@ import polars as pl
 
 def calculate_historical_expected_returns(df: pl.DataFrame, ids: list[str], frequency: int = 12) -> pl.DataFrame:
     """Calculate historical expected returns."""
-    count = df.shape[0]
+    years = frequency / df.shape[0]
     expected_returns = (
-        df.with_columns([(pl.col(id) + 1.0) for id in ids])
-        .select(ids)
+        df.drop("date")
+        .with_columns([(pl.col(id) + 1) for id in ids])
         .product()
-        .with_columns([(pl.col(id) ** (frequency / count) - 1) for id in ids])
+        .with_columns([(pl.col(id) ** (years) - 1) for id in ids])
     )
     return expected_returns
