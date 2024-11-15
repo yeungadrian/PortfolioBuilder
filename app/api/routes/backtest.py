c914d2d89b62220045470c0feb5f0ec5ece318b2
yeungadrian <yeungadrianjy@gmail.com>
2024-11-15 18:10:55 +0000

    move backtest logic out of api

--- app/api/routes/backtest.py
+++ app/api/routes/backtest.py
@@ -4,8 +4,8 @@ import polars as pl
 from fastapi import APIRouter, HTTPException
 
 from app.config import settings
-from app.loader import load_returns
 from app.models import BacktestResult, BacktestScenario, Holding, PortfolioValue
+from app.portfolio_analysis.backtest import run_backtesting
 from app.portfolio_analysis.metrics import get_portfolio_metrics
 
 router = APIRouter()
@@ -18,23 +18,6 @@ def get_invalid_ids(ids: list[str]) -> list[str]:
     )
     not_available = [_id for _id in ids if _id not in available_ids]
     return not_available
-
-
-def run_backtesting(backtest_scenario: BacktestScenario) -> pl.DataFrame:
-    """Run backtesting."""
-    ids = [i.id for i in backtest_scenario.portfolio]
-    security_returns = load_returns(ids, backtest_scenario.start_date, backtest_scenario.end_date)
-    # Set return for day 0 to 0.
-    security_returns = security_returns.with_columns(
-        [pl.when(pl.col("date") == pl.col("date").min()).then(0).otherwise(pl.col(_id)).alias(_id) for _id in ids]
-    )
-    # Calculate monthly returns
-    security_returns = security_returns.with_columns([(pl.col(_id) + 1.0).cum_prod().alias(_id) - 1.0 for _id in ids])
-    security_returns = security_returns.with_columns(
-        [((pl.col(holding.id) + 1.0) * holding.amount).alias(holding.id) for holding in backtest_scenario.portfolio]
-    )
-    security_returns = security_returns.with_columns(pl.sum_horizontal(ids).alias("portfolio_value"))
-    return security_returns
 
 
 @router.post("")
---
+++ app/portfolio_analysis/backtest.py
@@ -0,0 +1,21 @@
+import polars as pl
+
+from app.loader import load_returns
+from app.models import BacktestScenario
+
+
+def run_backtesting(backtest_scenario: BacktestScenario) -> pl.DataFrame:
+    """Run backtesting."""
+    ids = [i.id for i in backtest_scenario.portfolio]
+    security_returns = load_returns(ids, backtest_scenario.start_date, backtest_scenario.end_date)
+    # Set return for day 0 to 0.
+    security_returns = security_returns.with_columns(
+        [pl.when(pl.col("date") == pl.col("date").min()).then(0).otherwise(pl.col(_id)).alias(_id) for _id in ids]
+    )
+    # Calculate monthly returns
+    security_returns = security_returns.with_columns([(pl.col(_id) + 1.0).cum_prod().alias(_id) - 1.0 for _id in ids])
+    security_returns = security_returns.with_columns(
+        [((pl.col(holding.id) + 1.0) * holding.amount).alias(holding.id) for holding in backtest_scenario.portfolio]
+    )
+    security_returns = security_returns.with_columns(pl.sum_horizontal(ids).alias("portfolio_value"))
+    return security_returns
